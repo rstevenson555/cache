8d7185b7eb02643b5eaaeea2058f3c0ce6375f15
Stevenson, Bob (MONHC) <bob.stevenson@hyatt.com>
2016-04-26 08:47:31 -0500

    cleanup and fix group name

--- src/test/java/com/bos/tests/cache/NewTestSuite.java
+++ src/test/java/com/bos/tests/cache/NewTestSuite.java
@@ -36,39 +36,4 @@ public class NewTestSuite {
     public void tearDown() throws Exception {
     }
 
-    @Test
-    public void testInkAndToner() throws Exception {
-        MRUCache<String,HashMap> instance = new MRUCache<String,HashMap>(1,new AgeExpiringFactory<String,HashMap>(60*1000));
-
-        HashMap<String,String> cache = new HashMap<String,String>();
-        cache.put("Ink","Ink");
-        cache.put("printer ink","printer ink");
-        cache.put("toner cartridges","toner cartridges");
-        cache.put("printer cartridges","printer cartridges");
-        cache.put("HP toner","HP toner");
-        cache.put("Xerox refill","Xerox refill");
-        cache.put("toner ink","toner ink");
-        cache.put("printer refill","printer refill");
-        cache.put("printer refills","printer refills");
-        cache.put("toner cartridge","toner cartridge");
-        cache.put("printer cartridge","printer cartridge");
-        cache.put("toner refill","toner refill");
-        cache.put("toner refills","toner refills");
-        cache.put("guybrown reman","guybrown reman");
-        cache.put("guy brown remanufactured","guy brown remanufactured");
-
-        //instance.putAll(cache);
-        instance.put("cache", cache);
-
-        //String val = instance.get("printer refill");
-        HashMap map = instance.get("cache");
-        String val = (String)map.get("printer refill");
-        System.out.println("val: " + val);
-        System.out.println("val: " + cache.size());
-        System.out.println("val: " + instance.size());
-
-        //assert(val.equals("printer refill"));
-
-    }
-
 }
